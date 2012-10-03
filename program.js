9a2547bb44e44b1f013ce974bc1f64c336424e24
abutzier <abutzier@gmail.com>
2012-10-03 16:09:25 -0400

    stupid

--- program.js
+++ program.js
@@ -2,14 +2,29 @@ var fade = function(node) {
 	var level = 1;
 	var step = function ( ) {
 		var hex = level.toString(16);
+		var up = true;
 		node.style.backgroundColor = '#FFFF' + hex + hex;
-		if (level < 15) {
+		if (level < 15 && up) {
 			level += 1;
-			setTimeout(step, 100);
+			print('up');
 		}
+		else if (level === 15) {
+			up = false;
+			print('turn down');
+		}
+		else if (!up && level > 0) {
+			level -= 1;
+			print('down');
+		}
+		else if (level == 0) {
+			up = true;
+			print('turn up');
+		}
+		setTimeout(step, 100);
 	};
 	setTimeout(step, 100);
 };
+
 fade(document.body);
 
 var print = function(message) {
