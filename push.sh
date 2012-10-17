5c42ba9d8d390c66523c9c6017a459ac85d64f71
mihai <mihaibulic@gmail.com>
2012-10-16 22:42:46 -0400

    finalized push script

--- push.sh
+++ push.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-ssh ec2-user@ec2-184-72-242-128.compute-1.amazonaws.com 'cd /var/lib/tomcat6/webapps/ROOT/ramit && git pull github master'
+DEF_REMOTE="github";
+DEF_BRANCH="master";
+
+if [ $# -ne 0 ] && [ $# -ne 1 ] && [ $# -ne 2 ]
+then
+  echo "Error: use no args to push to the ${DEF_REMOTE} remote and ${DEF_BRANCH} branch OR enter just the branch name OR enter the remote followed by branch to use.";
+  echo "e.g.: $0 ${DEF_REMOTE} ${DEF_BRANCH}";
+  echo "e.g.: $0 ${DEF_BRANCH}";
+  echo "e.g.: $0";
+  exit
+fi
+
+if [ $# -eq 0 ] 
+then
+  remote="${DEF_REMOTE}";
+  branch="${DEF_BRANCH}";
+elif [ $# -eq 1 ]
+then
+  remote="${DEF_REMOTE}";
+  branch="${1}";
+else
+  remote="${1}";
+  branch="${2}";
+fi
+
+ssh ec2-user@ec2-184-72-242-128.compute-1.amazonaws.com 'cd /var/lib/tomcat6/webapps/ROOT/ramit && git pull ${remote} ${branch}'
 
 
