d7ff269eef943d1199eff3764b5a88a59b4e8333
mihai <mihaibulic@gmail.com>
2012-10-25 15:03:08 -0400

    small fix to node.js server

--- helloworldServer.js
+++ helloworldServer.js
@@ -1,4 +1,5 @@
-teServer(function (req, res) {
+var http = require('http');
+http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
