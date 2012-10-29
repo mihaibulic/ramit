f38abdca9e597d776f0db8c832bde8786b27c37b
EC2 Default User <ec2-user@domU-12-31-39-09-26-90.compute-1.internal>
2012-10-29 02:03:39 +0000

    Test stuff

--- server/testserver.js
+++ server/testserver.js
@@ -1,30 +1,17 @@
 /**
  * @author mihai
  */
-
-var app = require('http').createServer(function(req, res) {});
-var io = require('socket.io').listen(app);
+var io = require('socket.io').listen(1337);
 var s = 0;
 
-app.listen(1337, '127.0.0.1');
+io.sockets.on('connection', function(socket) {
+  setInterval(function() {
+    socket.emit('state', {score : s});
+    console.log("STATE SENT");
+  }, 1000);
 
-io.sockets.on('connection', 
-	function(socket) 
-	{
-		setInterval(
-			function()
-			{
-				socket.emit('state', {score : s});
-				console.log("STATE SENT");
-			},
-		1000);
-
-		socket.on('action',
-			function(data)
-			{
-				console.log("ACTION RECEIVED (" + s + ")");
-				s += data.change;
-			}
-		);
-	}
-);
+  socket.on('action', function(data) {
+    console.log("ACTION RECEIVED (" + s + ")");
+    s += data.change;
+  });
+});
