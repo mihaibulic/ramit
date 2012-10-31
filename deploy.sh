039305e05455cf3cc637a05805c43a389ddcf36a
mihai <mihaibulic@gmail.com>
2012-10-31 15:29:58 -0400

    made compile script into a deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+sudo service node stop
+
 # used to cd to the dir where this script is stored
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cat client/*.js shared/*.js > ramit_client.js
 cat shared/*.js server/*.js > ramit_server.js
+
+sudo service node start
+
