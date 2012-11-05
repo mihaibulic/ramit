7b21db23722316aad12b61e8fa932d3612ce1093
abutzier <abutzier@ubuntu.(none)>
2012-11-05 18:11:35 -0500

    A

--- shared/Projectile.js
+++ shared/Projectile.js
@@ -19,7 +19,7 @@ Projectile.prototype.draw = function(level) {
 	if (xPos > -20 && xPos < 1000 && yPos > -20 && yPos < 500) {
 		globals.ctx.fillStyle = "#ff0000";
 		globals.ctx.fillRect(xPos, yPos, 20, 20);
-		globals.ctx.strokeStyle("#00ff00");
+		globals.ctx.strokeStyle("#0000ff");
 		var rect = this.getCollisionBarrier();
 		globals.ctx.strokeRect(rect.left - level.x, rect.top - level.y, rect.width(),
 				rect.height());
