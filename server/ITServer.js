78afeeffbda9a0fd86d2a09e57c408337e0430be
abutzier <abutzier@ubuntu.(none)>
2012-11-05 20:27:34 -0500

    test

--- server/ITServer.js
+++ server/ITServer.js
@@ -44,6 +44,7 @@ var update = function() {
 	}
 	for (var projectile in server.projectiles) {
 		server.projectiles[projectile].update(server.level);
+		//fails on next line
 		//var target = server.projectiles[projectile].checkHit(globals, server.level);
 		//if (target >= -1) { 
 			//if (target >= 0) { 
