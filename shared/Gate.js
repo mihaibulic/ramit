eaf9db102f93fd93e87e64b6abbf9fe38c4884e1
Alex <abutzier@gmail.com>
2012-11-08 15:08:55 -0500

    test

--- shared/Gate.js
+++ shared/Gate.js
@@ -11,15 +11,16 @@ Gate.prototype.takeHit = function(damage) {
 }
 
 Gate.prototype.draw = function(level) {
-    var xPos = Gate.BARRIER[this.team].x - level.x;
-    var yPos = Gate.BARRIER[this.team].y - level.y;
+	var box = this.getCollisionBarrier();
+    var xPos = box.x - level.x;
+    var yPos = box.y - level.y;
 
     //if (xPos > -300 && xPos < 1000 && yPos > -20 && yPos < 500) {
 		globals.ctx.strokeStyle = Player.COLLISION_BOUND_STROKE[this.team];
 		globals.ctx.fillStyle = Player.COLLISION_BOUND_STROKE[this.team];
-		globals.ctx.strokeRect(xPos + 10, yPos + 40, 40, 10);
+		globals.ctx.strokeRect(xPos, yPos, box.width(), box.height());
 		globals.ctx.globalAlpha = this.health / 1000;
-		globals.ctx.fillRect(xPos + 10, yPos + 40, 40 * this.health / this.initHealth, 10);
+		golbals.ctx.fillRect(xPos, yPos, box.width(), box.height());
 		globals.ctx.globalAlpha = 1;
 	//}
 }
