c70dab067a65f9085c3b9a721b57f69a88bb71be
Alex <abutzier@gmail.com>
2012-11-07 22:04:23 -0500

    Test

--- shared/Player.js
+++ shared/Player.js
@@ -108,8 +108,8 @@ Player.prototype.draw = function(level) {
         // health bar
         globals.ctx.fillStyle = "#00ff00";
         globals.ctx.strokeStyle = "#00ff00";
-        globals.ctx.globalAlpha = 0.4;
         globals.ctx.strokeRect(xPos + 10, yPos + 40, 40, 10);
+        globals.ctx.globalAlpha = 0.4;
         globals.ctx.fillRect(xPos + 10, yPos + 40, 40 * this.health / this.initHealth, 10);
         globals.ctx.globalAlpha = 1;
     }
