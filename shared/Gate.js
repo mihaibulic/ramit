c1a44ac5ee67721be0de41c07670e8bb46b3e412
Alex <abutzier@gmail.com>
2012-11-08 15:11:20 -0500

    test

--- shared/Gate.js
+++ shared/Gate.js
@@ -22,6 +22,7 @@ Gate.prototype.draw = function(level) {
 		globals.ctx.globalAlpha = this.health / 1000;
 		golbals.ctx.fillRect(xPos, yPos, box.width(), box.height());
 		globals.ctx.globalAlpha = 1;
+		consol.log("drew gate %d at %d %d", this.team, xPos, yPos);
 	//}
 }
 
