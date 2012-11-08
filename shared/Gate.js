32ed4643529667764f8f3283569bef44a4e6246c
Alex <abutzier@gmail.com>
2012-11-08 15:43:54 -0500

    Test

--- shared/Gate.js
+++ shared/Gate.js
@@ -5,7 +5,7 @@ var Gate = function(team) {
 
 Gate.BARRIER = [];
 var b1 = { left: 1350, right: 1650, 
-					top: 500, bottom: 540 }
+					top: 500, bottom: 540 };
 Gate.BARRIER[0] = new Rectangle(b1);
 Gate.BARRIER[1] = new Rectangel( { left: 1350, right: 1650, 
 				 	top: 2500, bottom:2540 } );
