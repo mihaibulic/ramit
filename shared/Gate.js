f20c22bec02bfd4a6a73f88454fd0e498b2b3d66
Ryan <rtechnix.nxt@gmail.com>
2012-11-08 17:54:14 -0500

    Draw gate imges

--- shared/Gate.js
+++ shared/Gate.js
@@ -1,39 +1,36 @@
 
 var Gate = function(team) {
-	this.health = 1000;
-	this.team = team;
-	this.left = 1350;
-	this.right = 1650;
-	if (team == 0) {
-		this.top = 500;
-		this.bottom = 540;
-	} else {
-		this.top = 2500;
-		this.bottom = 2540;
-	}
+    this.health = 1000;
+    this.team = team;
+    this.left = 1350;
+    this.right = 1650;
+    if (team == 0) {
+	this.top = 475;
+	this.bottom = 500;
+    } else {
+	this.top = 2500;
+	this.bottom = 2525;
+    }
 };
 
 Gate.prototype.takeHit = function(damage) {
-	this.health -= damage;
-	if (this.health < 0) this.health = 0;
+    this.health -= damage;
+    if (this.health < 0) this.health = 0;
 };
 
 Gate.prototype.draw = function(level) {
-	var box = this.getCollisionBarrier();
+    var box = this.getCollisionBarrier();
     var xPos = this.left - level.x;
     var yPos = this.top - level.y;
-
+    
     if (xPos > -300 && xPos < 1000 && yPos > -20 && yPos < 500) {
-		globals.ctx.strokeStyle = Player.COLLISION_BOUND_STROKE[this.team];
-		globals.ctx.fillStyle = Player.COLLISION_BOUND_STROKE[this.team];
-		globals.ctx.strokeRect(xPos, yPos, box.width(), box.height());
-		globals.ctx.globalAlpha = this.health / 1000;
-		globals.ctx.fillRect(xPos, yPos, box.width(), box.height());
-		globals.ctx.globalAlpha = 1;
-	}
+	if (health > 0)
+	    globals.ctx.drawImage(globals.resources.gates[this.team], xPox, yPos);
+	globals.ctx.drawImage(globals.resources.gates[2], xPox, yPos);
+    }
 };
 
 Gate.prototype.getCollisionBarrier = function() {
-	return new Rectangle(this);
+    return new Rectangle(this);
 };
 
