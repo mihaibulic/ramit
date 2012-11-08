c0d03257e52a873f197c03cb7bbd0c528822d63a
Alex <abutzier@gmail.com>
2012-11-08 15:03:13 -0500

    always draw gates

--- shared/Gate.js
+++ shared/Gate.js
@@ -14,14 +14,14 @@ Gate.prototype.draw = function(level) {
     var xPos = Gate.BARRIER[this.team].x - level.x;
     var yPos = Gate.BARRIER[this.team].y - level.y;
 
-    if (xPos > -300 && xPos < 1000 && yPos > -20 && yPos < 500) {
+    //if (xPos > -300 && xPos < 1000 && yPos > -20 && yPos < 500) {
 		globals.ctx.strokeStyle = Player.COLLISION_BOUND_STROKE[this.team];
 		globals.ctx.fillStyle = Player.COLLISION_BOUND_STROKE[this.team];
 		globals.ctx.strokeRect(xPos + 10, yPos + 40, 40, 10);
 		globals.ctx.globalAlpha = this.health / 1000;
 		globals.ctx.fillRect(xPos + 10, yPos + 40, 40 * this.health / this.initHealth, 10);
 		globals.ctx.globalAlpha = 1;
-	}
+	//}
 }
 
 Gate.prototype.getCollisionBarrier = function(location) {
