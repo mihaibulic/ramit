2200464f34246d977366d02b21b6c109a5e59ae8
Ryan <rtechnix.nxt@gmail.com>
2012-11-08 19:27:29 -0500

    Temporary bug fix

--- client/ITGame.js
+++ client/ITGame.js
@@ -3,113 +3,114 @@
  */
 var ITGame = function(team, playerID) {
     this.level = new Level();
-
+    
     globals.socket = io.connect('ws://www.misquares.com');
     globals.socket.on('setup', globals.bind(function(data) {
-
-    globals.socket.on('state', function(data) {
-      for(var id in data) {
-          if (data[id].key !== undefined) {
-          globals.players[id].keys.up = (data[id].key&1);
-          globals.players[id].keys.down = (data[id].key&2);
-          globals.players[id].keys.left = (data[id].key&4);
-          globals.players[id].keys.right = (data[id].key&8);
-          }
-          if (data[id].x !== undefined)
-          globals.players[id].tank.x = data[id].x;
-          if (data[id].y !== undefined)
-          globals.players[id].tank.y = data[id].y;
-          if (data[id].aim !== undefined)
-          globals.players[id].setAim(data[id].aim);
-      }
-    });
-
-    globals.socket.on('join', function(data) {
-      globals.players[data.i] = new Player(data.t, data.i);
-    });
-    globals.socket.on('leave', function(data) {
-      delete globals.players[data.i];
-    });
-
-    globals.socket.on('fire', function(data) {
-        console.log("new projectile %d", data.n);
-        globals.projectiles[data.n] = new Projectile(globals.players[data.i], data.n);
-    });
-    globals.socket.on('hit', function(data) {
-	if (data.t || data.t == 0) {
-	    var projectile = globals.projectiles[data.n];
-	    var hitter = globals.players[projectile.owner];
-	    if (hitter) {
-		//hitter still in game
-		hitter.score++;
-		hitter.totScore++;
+	globals.socket.on('state', function(data) {
+	    for(var id in data) {
+		if (data[id].key !== undefined) {
+		    globals.players[id].keys.up = (data[id].key&1);
+		    globals.players[id].keys.down = (data[id].key&2);
+		    globals.players[id].keys.left = (data[id].key&4);
+		    globals.players[id].keys.right = (data[id].key&8);
+		}
+		if (data[id].x !== undefined)
+		    globals.players[id].tank.x = data[id].x;
+		if (data[id].y !== undefined)
+		    globals.players[id].tank.y = data[id].y;
+		if (data[id].aim !== undefined)
+		    globals.players[id].setAim(data[id].aim);
 	    }
-	    if (data.i || data.i === 0) {
-		//hit player
-		var hit = globals.players[data.i];
-		hit.takeHit(projectile.damage);
-		console.log("projectile %d hit player %d health %d", data.n, data.i, hit.health);
-	    } else {
-		//hit gate
-		var hit = level.gates[data.t];
-		hit.takeHit(projectile.damage);
-		console.log("projectile %d hit gate %d health %d", data.n, data.t, hit.health);
-	    }
-        } else {
-            console.log("projectile %d hit a wall", data.n);
-        }
-        delete globals.projectiles[data.n];
-    });
-
-    this.team = data.p.t;
-    this.player = data.p.i;
-    for (var pid in data.s) {
-        globals.players[pid] = new Player(data.s[pid].t, pid);
-        globals.players[pid].tank.x = data.s[pid].x;
-        globals.players[pid].tank.y = data.s[pid].y;
-        globals.players[pid].setAim(data.s[pid].aim);
-        globals.players[pid].setKeyValue(data.s[pid].key);
-    }
-    
-    // Input events.
-    var keyEvent = globals.bind(function(e) {
-        if (!e)
-            e = window.event;
-        globals.players[this.player].updateKeys(e);
-    }, this);
-
-    var mouseEvent = globals.bind(function(e) {
-        if(!e)
-            e = window.event;
-        globals.players[this.player].updateMouse(e);
-    }, this);
-    window.addEventListener('keydown', keyEvent);
-    window.addEventListener('keyup', keyEvent);
-    window.addEventListener('mousedown', mouseEvent);
-    window.addEventListener('mouseup', mouseEvent);
-    window.addEventListener('mousemove', globals.bind(function(e) {
-        if (!e)
-            e = window.event;
-        globals.players[this.player].updateAim(e);
-    }, this));
-    
-    // FPS Stuff
-    this.fps = 0;
-    this.count = 0;
-    if (globals.queries.debug === "true") {
-        this.fpsCount = window.setInterval(globals.bind(function() {
-            this.fps = this.count;
-            this.count = 0;
-        }, this), 1000);
-    }
-    
-    // Game loop.
-    this.interval = window.setInterval(globals.bind(function() {
-        if (globals.queries.debug === "true")
-            this.count++;
-        this.update();
-        this.draw();
-    }, this), 16);
+	});
+	
+	globals.socket.on('join', function(data) {
+	    globals.players[data.i] = new Player(data.t, data.i);
+	});
+	globals.socket.on('leave', function(data) {
+	    delete globals.players[data.i];
+	});
+	
+	globals.socket.on('fire', function(data) {
+            console.log("new projectile %d", data.n);
+            globals.projectiles[data.n] = new Projectile(globals.players[data.i], data.n);
+	});
+	
+	var level = this.level;
+	globals.socket.on('hit', function(data) {
+	    if (data.t || data.t == 0) {
+		var projectile = globals.projectiles[data.n];
+		var hitter = globals.players[projectile.owner];
+		if (hitter) {
+		    //hitter still in game
+		    hitter.score++;
+		    hitter.totScore++;
+		}
+		if (data.i || data.i === 0) {
+		    //hit player
+		    var hit = globals.players[data.i];
+		    hit.takeHit(projectile.damage);
+		    console.log("projectile %d hit player %d health %d", data.n, data.i, hit.health);
+		} else {
+		    //hit gate
+		    var hit = level.gates[data.t];
+		    hit.takeHit(projectile.damage);
+		    console.log("projectile %d hit gate %d health %d", data.n, data.t, hit.health);
+		}
+            } else {
+		console.log("projectile %d hit a wall", data.n);
+            }
+            delete globals.projectiles[data.n];
+	});
+	
+	this.team = data.p.t;
+	this.player = data.p.i;
+	for (var pid in data.s) {
+            globals.players[pid] = new Player(data.s[pid].t, pid);
+            globals.players[pid].tank.x = data.s[pid].x;
+            globals.players[pid].tank.y = data.s[pid].y;
+            globals.players[pid].setAim(data.s[pid].aim);
+            globals.players[pid].setKeyValue(data.s[pid].key);
+	}
+	
+	// Input events.
+	var keyEvent = globals.bind(function(e) {
+            if (!e)
+		e = window.event;
+            globals.players[this.player].updateKeys(e);
+	}, this);
+	
+	var mouseEvent = globals.bind(function(e) {
+            if(!e)
+		e = window.event;
+            globals.players[this.player].updateMouse(e);
+	}, this);
+	window.addEventListener('keydown', keyEvent);
+	window.addEventListener('keyup', keyEvent);
+	window.addEventListener('mousedown', mouseEvent);
+	window.addEventListener('mouseup', mouseEvent);
+	window.addEventListener('mousemove', globals.bind(function(e) {
+            if (!e)
+		e = window.event;
+            globals.players[this.player].updateAim(e);
+	}, this));
+	
+	// FPS Stuff
+	this.fps = 0;
+	this.count = 0;
+	if (globals.queries.debug === "true") {
+            this.fpsCount = window.setInterval(globals.bind(function() {
+		this.fps = this.count;
+		this.count = 0;
+            }, this), 1000);
+	}
+	
+	// Game loop.
+	this.interval = window.setInterval(globals.bind(function() {
+            if (globals.queries.debug === "true")
+		this.count++;
+            this.update();
+            this.draw();
+	}, this), 16);
     }, this));
 };
 
