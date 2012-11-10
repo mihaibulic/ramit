6d39e014aa119ed8f37a56c0ffba955332ac5134
mihai <mihaibulic@gmail.com>
2012-11-09 19:39:27 -0500

    rocket fix

--- client/ITGame.js
+++ client/ITGame.js
@@ -27,7 +27,7 @@ var ITGame = function(team, playerID) {
         }
         if (data[id].m !== undefined) {
           console.log("new mine %d", data[id].m);
-          if(data[id].mx === undefined)
+          if(data[id].mn === undefined)
             globals.mines[data[id].m] = new Mine(globals.players[id], data[id].m);
           else
             globals.mines[data[id].m] = new Mine(globals.players[id], data[id].m, globals.projectiles[data[id].mn]);
