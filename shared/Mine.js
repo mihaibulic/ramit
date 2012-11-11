2f911fb0368f2ff6d5f42287046947c0a94c1dd9
Alex <abutzier@gmail.com>
2012-11-10 20:21:18 -0500

    mine class redone

--- shared/Mine.js
+++ shared/Mine.js
@@ -3,55 +3,36 @@
  */
 var Mine = function(player, m) {
   this.owner = player.playerID;
+  this.team = player.team;
   this.m = m;
   this.x = player.tank.x+30; //center of mine
   this.y = player.tank.y+30;
   this.range = player.mine.range;
   this.damage = player.mine.damage;
-  this.delay = 300; //5 sec delay
-  this.splash = [];
 
   player.mine.live++;
 };
 
-Mine.prototype.update = function(globals) {
-  var hits = [];
+Mine.RADIUS = 10;
 
-  if (this.delay > 0) {
-    this.delay--;
-  }
-  else {
-    if(this.delay === 0) {
-      this.splash = new Splash(this.x, this.y, this.range.range, this.damage);
-      console.log("MINE %d IS LIVE!", this.m);
-    }
-
-    hits = splash.getHits(globals);
-  }
-
-  return hits;
-};
-
-Mine.prototype.draw = function(level) {
+Mine.prototype.draw = function(level, yourTeam) {
   var xPos = this.x - level.x;
   var yPos = this.y - level.y;
 
   if (xPos > -10 && xPos < 1000 && yPos > -10 && yPos < 500) {
-    if (this.delay > 0) {
-      globals.ctx.fillStyle = "#00FF00"; //green if not hot
-    } else {
-      globals.ctx.fillStyle = "#000000"; //black if hot
+    if (yourTeam === this.team) { //mines are invisible to enemies!
+      globals.ctx.fillStyle = Player.COLLISION_BOUND_STROKE[this.team];
+      globals.ctx.beginPath();
+      globals.ctx.arc(xPos, yPos, Mine.RADIUS, 0 , 2 * Math.PI, true);
+      globals.ctx.closePath();
+      globals.ctx.fill();
     }
-    globals.ctx.beginPath();
-    globals.ctx.arc(xPos, yPos, 5, 0 , 2 * Math.PI, true);
-    globals.ctx.closePath();
-    globals.ctx.fill();
 
     if (globals.queries.debug === "true") {
       globals.ctx.strokeStyle = globals.ctx.fillStyle;
-      globals.ctx.strokeRect(xPos-5, yPos-5, 10, 10);
+      globals.ctx.strokeRect(xPos-Mine.RADIUS, yPos-Mine.RADIUS, Mine.RADIUS*2, Mine.RADIUS*2);
       globals.ctx.beginPath();
-      globals.ctx.arc(xPos, yPos, this.range + 5, 0, 2 * Math.PI, true);
+      globals.ctx.arc(xPos, yPos, this.range, 0, 2 * Math.PI, true);
       globals.ctx.closePath();
       globals.ctx.stroke();
     }
@@ -59,6 +40,6 @@ Mine.prototype.draw = function(level) {
 };
 
 Mine.prototype.getCollisionBarrier = function() {
-  return new Rectangle( { right: this.x + 5, left: this.x - 5,
-                          top: this.y - 5, bottom: this.y + 5} );
+  return new Rectangle( { right: this.x + Mine.RADIUS, left: this.x - Mine.RADIUS,
+                          top: this.y - Mine.RADIUS, bottom: this.y + Mine.RADIUS} );
 };
