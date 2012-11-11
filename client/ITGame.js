94b75fc4567a876a0ced3b19867dfbda51860731
Ryan <rtechnix.nxt@gmail.com>
2012-11-11 04:05:01 -0500

    Delete projectiles on disconnect

--- client/ITGame.js
+++ client/ITGame.js
@@ -11,10 +11,12 @@ var ITGame = function(team, playerID) {
 
     globals.socket.on('leave', function(data) {
       delete globals.players[data.i];
-      // Remove all projectiles owned by this player.
+      // Remove all projectiles and mines owned by this player.
       for (var qid in globals.projectiles) {
-        if (globals.projectiles[qid].owner === data.i)
+        if (globals.projectiles[qid].owner === data.i) {
           delete globals.projectiles[qid];
+          delete globals.mines[qid];
+        }
       }
     });
 
