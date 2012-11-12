cd3ea0fddc3fe9be97336d4365333ece7c2905f5
Ryan <rtechnix.nxt@gmail.com>
2012-11-11 19:33:12 -0500

    Kill keys on blur

--- client/ITGame.js
+++ client/ITGame.js
@@ -49,7 +49,7 @@ var ITGame = function(team, playerID) {
       window.console.log("BLUR");
       globals.socket.emit("key", {u:0,l:0,r:0,d:0,s:0,e:0,q:0});
       globals.socket.emit("mouse", {l:0,m:0,r:0});
-    });
+    };
     // Context Menu Event
     window.addEventListener('contextmenu', function(e) {
       if(!e)
