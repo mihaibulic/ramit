7cd3871000c2823f3a972d08c72da1610e7c4a2e
Alex <abutzier@gmail.com>
2012-11-11 19:18:56 -0500

    Test

--- server/ITServer.js
+++ server/ITServer.js
@@ -91,6 +91,7 @@ var update = function() {
     }
 
     // Special
+    console.log(player.mounted + " is mounted");
     if (player.special[player.mounted].lastFire > player.special[player.mounted].coolDown &&
         (player.mouse.right === true || player.keys.shift === true)) {
         if (player.mounted === Player.specialType.ROCKET) {
