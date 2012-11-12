8864602ae04e1fa5b5a8e149d9e2af30ebe8b78a
Alex <abutzier@gmail.com>
2012-11-11 22:19:43 -0500

    Test

--- shared/Gate.js
+++ shared/Gate.js
@@ -91,8 +91,8 @@ Gate.prototype.draw = function() {
         if (color == Player.HEALTH.length) color--;
         globals.ctx.fillStyle = Player.HEALTH[color];
         globals.ctx.globalAlpha = 0.5;
-        globals.ctx.strokeRect(1450-globals.level.x, yPos-2, 100, 3);
-        globals.ctx.fillRect(1450-globals.level.x, yPos-2, 100 * this.health / 1000, 3);
+        globals.ctx.strokeRect(xPos + 100, yPos-2, 100, 3);
+        globals.ctx.fillRect(xPos + 100, yPos-2, 100 * this.health / 1000, 3);
         globals.ctx.globalAlpha = 1;
          //name
         globals.ctx.fillStyle = "#FFFFFF";
