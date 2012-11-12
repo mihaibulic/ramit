a4013490a1cc547beb258be40e6148be50caa2db
mihai <mihaibulic@gmail.com>
2012-11-12 15:07:40 -0500

    testing onblur

--- client/ITGame.js
+++ client/ITGame.js
@@ -46,6 +46,7 @@ var ITGame = function(team, playerID) {
 
     //Blur event
     document.body.onblur = function(e) {
+      console.log("CLEAR");
       globals.socket.emit("key", {u:0,l:0,r:0,d:0,s:0,e:0,q:0});
       globals.socket.emit("mouse", {l:0,m:0,r:0});
     };
