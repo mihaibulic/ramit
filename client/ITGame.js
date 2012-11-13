b50e0e7f393b13e51bf03cd56925b0e6182b5092
Alex <abutzier@gmail.com>
2012-11-12 21:08:41 -0500

    Test

--- client/ITGame.js
+++ client/ITGame.js
@@ -206,6 +206,7 @@ ITGame.prototype.update = function() {
   for (var pid in globals.players) {
     if (globals.players[pid].leaving) {
       console.log(globals.players[pid].name + " is in the process of leaving");
+      globals.players[pid].deathCounter++;
       console.log("deathCounter = " + globals.players[pid].deathCounter);
     }
     if (globals.players[pid].leaving && globals.players[pid].deathCounter >= 90) {
