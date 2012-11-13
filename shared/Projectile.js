42f2d35729151c481480a69c43b5ea3b0af37f6e
Alex <abutzier@gmail.com>
2012-11-13 09:43:10 -0500

    test

--- shared/Projectile.js
+++ shared/Projectile.js
@@ -206,9 +206,6 @@ Projectile.prototype.getCollisionBarrier = function() {
   if (this.type === Projectile.Type.MINE)
     return new Rectangle({left: this.x - 10, right: this.x + 10,
                           top: this.y - 10, bottom: this.y + 10});
-  else if (this.typw === Projectile.Type.ROCKET)
-    return new Rectangle({left: this.x - 20, right: this.x + 20,
-                          top: this.y - 20, bottom: this.y + 20});
   return new Rectangle({left: this.x - 5, right: this.x + 5,
                         top: this.y - 5, bottom: this.y + 5});
 };
