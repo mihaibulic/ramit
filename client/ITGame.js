30db1a6d4e06320da346a0aa2eb0791d0474a273
mihai <mihaibulic@gmail.com>
2012-11-13 02:45:54 -0500

    new player test

--- client/ITGame.js
+++ client/ITGame.js
@@ -11,11 +11,9 @@ var ITGame = function(team, playerID) {
 
     globals.socket.on('join', function(data) {
       console.log("join msg rec");
-      if (!globals.players[data.i]) {
-        var player = new Player(null, data.i, data.p);
-        globals.players[data.i] = player;
-        globals.messages.push("*****************" + player.name + " has joined the game for the " + (player.team === 0 ? "Blue" : "Red") + " Team");
-      }
+      var player = new Player(null, data.i, data.p);
+      globals.players[data.i] = player;
+      globals.messages.push("*****************" + player.name + " has joined the game for the " + (player.team === 0 ? "Blue" : "Red") + " Team");
     });
 
     globals.socket.on('leave', function(data) {
