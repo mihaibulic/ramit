fbff566902bd523131f79a4520832f8e32178381
mihai <mihaibulic@gmail.com>
2012-11-13 02:52:14 -0500

    join fix

--- client/ITGame.js
+++ client/ITGame.js
@@ -10,10 +10,9 @@ var ITGame = function(team, playerID) {
     }, this));
 
     globals.socket.on('join', function(data) {
-      console.log("join msg rec");
       var player = new Player(null, data.i, data.p);
       globals.players[data.i] = player;
-      globals.messages.push("*****************" + player.name + " has joined the game for the " + (player.team === 0 ? "Blue" : "Red") + " Team");
+      globals.messages.push(player.name + " has joined the game for the " + (player.team === 0 ? "Blue" : "Red") + " Team");
     });
 
     globals.socket.on('leave', function(data) {
