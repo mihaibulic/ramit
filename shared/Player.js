e0e9453a9e14b32479e06c721554710d7f8c2276
abutzier <abutzier@gmail.com>
2012-11-13 22:03:46 -0500

    test

--- shared/Player.js
+++ shared/Player.js
@@ -537,13 +537,13 @@ Player.prototype.predict = function(data) {
   } else {
     if (this.tank.sx !== this.tank.x) {
       var diff = Math.abs(this.tank.sx - this.tank.x);
-      if (diff < 20 || diff > 100 || diff < 200)
+      if (diff < 20 || diff > 100 || true)
         this.tank.x = this.tank.sx;
       else 
         this.tank.x = (this.tank.x + this.tank.sx) / 2;
-    } if (this.tank.sy !== this.tank.sy) {
+    } if (this.tank.sy !== this.tank.y) {
       var diff = Math.abs(this.tank.sy - this.tank.y);
-      if (diff < 20 || diff > 100 || diff < 200) 
+      if (diff < 20 || diff > 100 || true) 
         this.tank.y = this.tank.sy;
       else
         this.tank.y = (this.tank.y + this.tank.sy) / 2;
