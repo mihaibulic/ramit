f302a016ce15daccfb6cb3e4e6e91203a0e87372
mihai <mihaibulic@gmail.com>
2012-11-14 18:09:01 -0500

    key clear test

--- shared/Player.js
+++ shared/Player.js
@@ -504,13 +504,6 @@ Player.prototype.predict = function(data, you) {
       this.team = data.t;
     if (data.h !== undefined) {
       if (this.health !== data.h && data.h === 0) {
-        if (this.playerID === you) {
-          globals.keys = {up:0, down:0, left:0, right:0, mine:0, all_mines:0, space:0, shift:0, mounted:0};
-          globals.mouse = {left:0, middle:0, right:0};
-
-          globals.socket.emit("key", {u:0,d:0,l:0,r:0,e:0,q:0,s:0,w:0,m:0});
-          globals.socket.emit("mouse", {l:0,m:0,r:0});
-        }
         var deadName = (this.playerID != you ? this.name : "You");
         var killer = globals.players[data.b];
         if (killer === undefined)
@@ -804,6 +797,11 @@ Player.prototype.respawn = function() {
   this.tank.x = Player.SPAWN_POINTS[this.team][spawn].x;
   this.tank.y = Player.SPAWN_POINTS[this.team][spawn].y;
   this.health = this.maxHealth;
+
+  globals.keys = {up:0, down:0, left:0, right:0, mine:0, all_mines:0, space:0, shift:0, mounted:0};
+  globals.mouse = {left:0, middle:0, right:0};
+  globals.socket.emit("key", {u:0,d:0,l:0,r:0,e:0,q:0,s:0,w:0,m:0});
+  globals.socket.emit("mouse", {l:0,m:0,r:0});
 
   if (globals.diff) {
     if (!globals.diff.p)
