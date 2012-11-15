57251d8a90d93bf2a31934fd11e873a00206907d
Ryan <rtechnix.nxt@gmail.com>
2012-11-14 19:47:46 -0500

    Moving to time based rather than frame based movement

--- client/main.js
+++ client/main.js
@@ -371,3 +371,17 @@ window.onload = function() {
     globals.game = new ITGame(0,0);
   });
 };
+
+/**
+ * Set requestAnimationFrame in a cross-browser way.
+ */
+if ( !window.requestAnimationFrame ) {
+  window.requestAnimationFrame = ( function() {
+    return  window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) {
+        window.setTimeout( callback, 1000 / 60, new Date()); };
+  } )();
+}
