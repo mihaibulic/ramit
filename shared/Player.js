f0624a7c63b1d019a3dd3d664fa11cfe408be330
abutzier <abutzier@gmail.com>
2012-11-19 14:06:01 -0500

    Test

--- shared/Player.js
+++ shared/Player.js
@@ -326,9 +326,8 @@ Player.prototype.drawHUD = function() {
   globals.ctx.strokeStyle = Player.HEALTH[color];
   globals.ctx.globalAlpha = 0.75;
   drawRoundRect(globals.ctx, 55, 20, 300, 20, 10);
-  globals.ctx.lineWidth = 3;
-  globals.ctx.stroke();
   globals.ctx.lineWidth = 1;
+  globals.ctx.stroke();
   drawRoundRect(globals.ctx, 55, 20, 300 * this.health / this.maxHealth, 20, 10);
   globals.ctx.fill();
   globals.ctx.globalAlpha = 1;
