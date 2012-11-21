32b6bc5aa64ac4ee4a9f0c1381f5c8ed75c84c5b
mihai <mihaibulic@gmail.com>
2012-11-20 23:35:05 -0500

    gray out bomb after fire

--- shared/Player.js
+++ shared/Player.js
@@ -333,21 +333,19 @@ Player.prototype.drawHUD = function() {
 
   // Special weapons + cooldowns
   globals.ctx.globalAlpha = 0.75;
-  globals.ctx.fillStyle = "#8a8a8a";
-  drawRoundRect(globals.ctx, 15 + 40*(this.mounted), 45, 40, 40);
-  globals.ctx.fill();
-  globals.ctx.fillStyle = Player.TEAM_COLOR[this.team]; 
-
+  if(this.special[this.mounted].allowed > 0) {
+    globals.ctx.fillStyle = "#8a8a8a";
+    drawRoundRect(globals.ctx, 15 + 40*(this.mounted), 45, 40, 40);
+    globals.ctx.fill();
+    globals.ctx.fillStyle = Player.TEAM_COLOR[this.team]; 
+  }
   for (var s in this.special) {
     globals.ctx.fillRect(20 + 40*(s), 50, 30, 30);
   
     globals.ctx.fillStyle = "#8a8a8a"; 
-    if (this.special[s].allowed <= 0) { 
-      // if this weapon is not allowed, draw a grayed out box on top of it
+    if (this.special[s].allowed <= 0)  
       globals.ctx.fillRect(20 + 40*(s), 50, 30, 30);
-    }
     else if (this.special[s].lastFire < this.special[s].coolDown) { 
-      // draw cooldown 
       var coolDownPercent = ((this.special[s].coolDown - this.special[s].lastFire) / this.special[s].coolDown);
       globals.ctx.fillRect(20 + 40*(s), 50 + (30*(1-coolDownPercent)), 30, 30 * coolDownPercent);
     }
@@ -718,7 +716,6 @@ Player.prototype.update = function() {
     this.special[Player.SpecialType.MEDIC].lastFire++;
     if (this.hasShield === 0)
       this.special[Player.SpecialType.SHIELD].lastFire++;
-
   }
   if (this.hasShield) {
     this.hasShield--;
