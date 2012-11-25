5b8475c38e6c841b9d0b227cfa8ce00800707619
mihai <mihaibulic@gmail.com>
2012-11-24 23:59:37 -0500

    shield

--- shared/Player.js
+++ shared/Player.js
@@ -663,11 +663,11 @@ Player.prototype.loadState = function(data, you) {
         if (data.d <= 0 && !this.special[Player.SpecialType.SHIELD].inUse.isDone()) { 
           // server has shield off, but client has it on      
           this.special[Player.SpecialType.SHIELD].inUse.reset();
-          this.special[Player.SpecialType.SHIELD].lastFire.reset();
         }
         else if (data.d > 0 && this.special[Player.SpecialType.SHIELD].inUse.isDone()) {
           // server has shield on, but client doesn't
           this.special[Player.SpecialType.SHIELD].inUse.tempSet(data.d);
+          this.special[Player.SpecialType.SHIELD].lastFire.reset();
         }
     }
     if (data.p !== undefined)
