4550dfede91a1be26a7dc583c76c780e5837c525
mihai <mihaibulic@gmail.com>
2012-11-25 00:00:44 -0500

    cooldown test

--- shared/Player.js
+++ shared/Player.js
@@ -33,7 +33,7 @@ var Player = function(team, playerID, opt_state) {
     range: 40,
     damage: 20,
     speed: 9 * 60,
-    coolDown: 2000,
+    coolDown: 20000,
     lastFire: new Timer(2000),
     allowed: 1 // not used to limit how many rockets are out there, just to say the weapon is permitted
   };
