aab9b3ccdd16a3f437ab74f7c833d230e9b02c63
mihai <mihaibulic@gmail.com>
2013-01-12 21:25:28 -0500

    log fix

--- getLog.sh
+++ getLog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -i misquares.pem ec2-user@ec2-184-72-242-128.compute-1.amazonaws.com:/tmp/node.log .
+scp -i misquares.pem ec2-user@ec2-184-72-242-128.compute-1.amazonaws.com:/tmp/ramit_node.log node.log 
 
 echo "*****************************************************************";
 if [ $? -eq 1 ]; then
